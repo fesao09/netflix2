7b0c205838bade63676809d100cd63f74e74d4e8
Felipe Oliveira <felipe.oliveira@ZBN.GROUP>
2024-12-12 17:09:04 -0300

    altera

--- public/script.js
+++ public/script.js
@@ -15,8 +15,8 @@ const db = firebase.firestore();
 console.log('Firebase initialized:', firebaseConfig);
 
 // TMDB configuration
-const tmdbApiKey = process.env.TMDB_API_KEY || '5c66fecac3410a4da2709f1d944be38c'; // Usar variável de ambiente ou chave de fallback
-const tmdbImageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+const tmdbApiKey = window.tmdbApiKey || '5c66fecac3410a4da2709f1d944be38c'; // Usar variável de ambiente ou chave de fallback
+const tmdbImageBaseUrl = window.tmdbImageBaseUrl || 'https://image.tmdb.org/t/p/w500';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Document loaded');
