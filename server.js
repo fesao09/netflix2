2bf4c227dfa34c2f2a69da69b622cc7831168000
Felipe Oliveira <felipe.oliveira@ZBN.GROUP>
2024-12-12 11:55:21 -0300

    Alteração server por conta do Firebase

--- server.js
+++ server.js
@@ -6,20 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Verificar se as variáveis de ambiente estão definidas
-if (!process.env.FIREBASE_API_KEY || !process.env.FIREBASE_AUTH_DOMAIN || !process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_STORAGE_BUCKET || !process.env.FIREBASE_MESSAGING_SENDER_ID || !process.env.FIREBASE_APP_ID) {
-  console.error('Variáveis de ambiente do Firebase não estão definidas');
-  process.exit(1);
-}
-
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyDs877-qqhixjzOtODzNEe66zJjRgG0ho0",
+  authDomain: "netflix2-40dbe.firebaseapp.com",
+  projectId: "netflix2-40dbe",
+  storageBucket: "netflix2-40dbe.appspot.com",
+  messagingSenderId: "272948667951",
+  appId: "1:272948667951:web:eb84b6fd3327eaacaa08da",
+  measurementId: "G-D8ZQWFDDWT"
 };
 
 // Initialize Firebase
