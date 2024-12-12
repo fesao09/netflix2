03a7f8387360b3839b96359d34bd7c2a4ffa0d62
fesao09 <feoliveira339@gmail.com>
2024-12-12 10:24:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Error occurred:', err.message);
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
